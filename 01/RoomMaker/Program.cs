f5051b52c417864f8896d8b31b7260a3fa02f9b0
Simplifier <iseldomused@gmail.com>
2020-02-04 22:06:43 +0300

    first task

---
+++ 01/RoomMaker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security;
+
+namespace RoomMaker
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Room room = new Room(new[] {new Thing("table", 500), new Thing("chair", 100)});
+
+            AssertEqual(room.Contains("table"), true);
+            AssertEqual(room.Contains("chair"), true);
+            AssertEqual(room.Contains("cabinet"), false);
+            AssertEqual(room.GetCost(), 600);
+
+            Console.WriteLine("No errors");
+            Console.Read();
+        }
+
+        private static void AssertEqual<T>(T actual, T expected)
+        {
+            if (!actual.Equals(expected))
+            {
+                throw new VerificationException($"expected: {expected}, but actual: {actual}");
+            }
+        }
+    }
+}
